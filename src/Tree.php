345a6b15b124f04371c42d083ded75a5cea2f582
wfy2016 <1541123657@qq.com>
2017-11-15 10:39:23 +0800

    composer on git

---
+++ src/Tree.php
@@ -0,0 +1,12 @@
+<?php 
+	namespace Acme;
+echo "<BR>Space\n";
+
+class Tree
+{
+    public function leaves($value='')
+    {
+        echo "<BR>Space leaves\n";
+    }
+}//end class
+?>
---
+++ time/Tree.php
@@ -0,0 +1,12 @@
+<?php
+namespace Time;
+echo "<BR>Time\n";
+
+class Tree
+{
+
+    public function leaves($value='')
+    {
+        echo '<BR>Time leaves';
+    }
+}//end class
